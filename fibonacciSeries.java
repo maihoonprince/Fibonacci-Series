75d32a96ab4154020780fdff7b195e6ea542271b
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-01-30 00:12:39 +0530

    Add files via upload

---
+++ fibonacciSeries.java
@@ -0,0 +1,23 @@
+import java.rmi.StubNotFoundException;
+import java.util.*;
+
+public class fibonacciSeries 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+        for(int i=0; i<=n; i++)
+        {
+            System.out.println(a);
+            int c=a+b;
+            a = b;
+            b = c;
+
+        }
+    }
+    
+}
